9a7525fbf130509057200c5bcd01878180f2d243
Alex Harvill <alex.harvill@gmail.com>
2013-07-21 19:08:13 -0700

    Support arbitrary channel ordering using shuffleChannels.  Removed legacy setup.py config.

--- imagetask/test/test.py
+++ imagetask/test/test.py
@@ -5,16 +5,21 @@ from pprint import pprint
 import logging
 logging.getLogger().setLevel(0)
 
-path = os.path.dirname(os.path.abspath(__file__))+'/xpos.exr'
+def go( path):
+    info = exrinfoext.ExrInfoExt( path, read_buffer=True )
 
-info = exrinfoext.ExrInfoExt( path, read_buffer=True )
-pprint(info.attributes)
+    import numpy
+    numpy.set_printoptions( suppress=True )
+    pprint(info.__dict__)
+    if info.buffer is not None:
+        from PyQt4 import QtGui
 
-import numpy
-numpy.set_printoptions( suppress=True )
-print( 'worldToNDC')
-print( info.attributes['worldToNDC'] )
-if info.buffer is not None:
-    from PyQt4 import QtGui
-    result = QtGui.QImage(info.buffer.data, info.height, info.width, QtGui.QImage.Format_RGB32)
-    result.save('result.png')
+        info.shuffleChannels([2,1,0,3])
+
+        result = QtGui.QImage(info.buffer.data, info.width, info.height, QtGui.QImage.Format_ARGB32_Premultiplied)
+
+        result.save(path.replace('exr','png'))
+
+import glob
+for path in glob.glob(os.path.dirname(os.path.abspath(__file__))+'/*.exr'):
+    go(path)
